7a6effc2782b488f755d766d17da4f9cd3b9548c
MarekRuzic <96700829+MarekRuzic@users.noreply.github.com>
2025-10-05 12:46:11 +0200

    Přidejte soubory projektu.

---
+++ WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Plugin.Firebase.CloudMessaging;
+
+namespace WeatherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            /*count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);*/
+
+            await CrossFirebaseCloudMessaging.Current.CheckIfValidAsync();
+            var token = await CrossFirebaseCloudMessaging.Current.GetTokenAsync();
+            Console.WriteLine($"FCM token: {token}");
+        }
+    }
+
+}
---
+++ WeatherApp/MauiProgram.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+using Plugin.Firebase.Core.Platforms.Android;
+
+namespace WeatherApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.RegisterFirebaseServices()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+
+	private static MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder)
+	{
+
+        builder.ConfigureLifecycleEvents(events => {
+			events.AddAndroid(android => android.OnCreate((activity, _) =>
+			CrossFirebase.Initialize(activity)));
+        });
+
+        return builder;
+	}
+}
