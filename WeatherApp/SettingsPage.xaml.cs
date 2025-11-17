885a185dba666d48e53118a0f84e3a8a271a428d
MarekRuzic <96700829+MarekRuzic@users.noreply.github.com>
2025-11-17 20:56:06 +0100

    Notification in setting can be enabled

--- WeatherApp/SettingsPage.xaml.cs
+++ WeatherApp/SettingsPage.xaml.cs
@@ -4,51 +4,64 @@ namespace WeatherApp;
 
 public partial class SettingsPage : ContentPage
 {
-    private readonly NotificationService _notificationService;
+    private readonly NotificationService _notificationService = new NotificationService();
+    private bool isHandlingCheckChange = false;
 
     public SettingsPage()
     {
         InitializeComponent();
 
         BindingContext = new SettingsViewModel();
+        PageLoad();
+        App.AppResumed += (s, e) => PageLoad();
     }
-
-    public SettingsPage(NotificationService notificationService)
-	{
-		InitializeComponent();
-
-        BindingContext = new SettingsViewModel();
-    
-		_notificationService = notificationService;
-    }
-
 	
 
     private async void PageLoad()
 	{
+        isHandlingCheckChange = true;
 		if (await _notificationService.IsNotificationEnabledAsync())
 		{
 			NotificationImage.Source = "notification_bell_enabled.png";
+            CheckBoxNotificationCheck.IsChecked = true;
+            CheckBoxNotificationCheck.IsEnabled = false;
         }
 		else
 		{
             NotificationImage.Source = "notification_bell_disabled.png";
+            CheckBoxNotificationCheck.IsChecked = false;
+            CheckBoxNotificationCheck.IsEnabled = true;
         }
+        isHandlingCheckChange = false;
     }
 
-	private async Task<bool> IsNotificationEnable()
-	{
-		if (DeviceInfo.Platform != DevicePlatform.Android) return false;
-        var status = await Permissions.CheckStatusAsync<Permissions.PostNotifications>();
-		if (status != PermissionStatus.Granted)
-		{
-			return true;
-		}
-        return false;
-	}
-
-    private void CheckChange_Notification(object sender, EventArgs e)
+    
+    private async void CheckChange_Notification(object sender, EventArgs e)
     {
+        if (isHandlingCheckChange) return;
 
+        if (DeviceInfo.Platform != DevicePlatform.Android)
+        {
+            isHandlingCheckChange = false;
+            return;
+        }
+
+        isHandlingCheckChange = true;
+
+        bool openSettings = await DisplayAlert(
+                    "Notifikace zakázány",
+                    "Pro správné fungování aplikace prosím povolte notifikace v nastavení systému.",
+                    "Otevøít nastavení",
+                    "Zrušit");
+
+        if (openSettings)
+        {
+            AppInfo.ShowSettingsUI();
+        }
+        else
+        {
+            CheckBoxNotificationCheck.IsChecked = false;
+        }
+        isHandlingCheckChange = false;
     }
 }
