b1cdd4542300af52944c139da75d7c5274cb3b6b
MarekRuzic <96700829+MarekRuzic@users.noreply.github.com>
2025-11-18 22:14:41 +0100

    WeatherList - load only part

--- WeatherApp/ViewModel/WeatherListViewModel.cs
+++ WeatherApp/ViewModel/WeatherListViewModel.cs
@@ -18,11 +18,53 @@ namespace WeatherApp.ViewModel
 
         public ObservableCollection<WeatherRecord> WeatherItems { get; set; } = new ObservableCollection<WeatherRecord>();
 
+        private List<WeatherRecord> allItems;
+
+        public Command LoadMoreCommand { get; }
+
         public WeatherListViewModel()
         {
-            LoadFakeData();
+            //LoadFakeData();
+
+            LoadMoreCommand = new Command(LoadMore);
+
+            // Načti všechna data (API nebo fake)
+            allItems = LoadFakeDataList();
+
+            // Poprvé zobraz jen prvních 10
+            LoadMore();
         }
 
+        private List<WeatherRecord> LoadFakeDataList()
+        {
+            List<WeatherRecord> items = new List<WeatherRecord>();
+            for (int i = 0; i < 50; i++)
+            {
+                items.Add(new WeatherRecord
+                {
+                    Date = DateTime.Now.AddDays(-i),
+                    TemperatureC = Random.Shared.Next(-10, 35),
+                    Summary = WeatherCategoryExtensions.GetRandomSummary(),
+                    Icon = WeatherCategoryExtensions.GetImagePath(WeatherCategoryExtensions.GetRandomSummary())
+                });                
+            }
+            return items;
+        }
+
+        private void LoadMore()
+        {
+            var nextItems = allItems
+                .Skip(loadedCount)
+                .Take(batchSize)
+                .ToList();
+
+            foreach (var item in nextItems)
+                WeatherItems.Add(item);
+
+            loadedCount += nextItems.Count;
+        }
+
+        // Původní
         private void LoadFakeData()
         {
             for (int i = 0; i < 50; i++)
